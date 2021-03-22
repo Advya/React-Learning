e02e95a735e10d55f11a23d2d8137f9d16793aad
advya <50411324+Advya@users.noreply.github.com>
2021-03-22 13:21:34 +0200

    task 3

--- React-test.js
+++ React-test.js
@@ -39,3 +39,27 @@ class Calendar extends React.Component {
     );
   }
 };
+
+// Pass an Array as PropsPassed
+
+const List = props => {
+  return <p>{props.tasks.join(", ")}</p>;
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        <List tasks={["Walk", "Cook", "Bake"]} />
+        <h2>Tomorrow</h2>
+        <List tasks={["Study", "Code", "Eat"]} />
+      </div>
+    );
+  }
+}
+
