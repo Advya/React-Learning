ad77dba78607f6ee51c1eb30c21ec0ad08d26905
advya <50411324+Advya@users.noreply.github.com>
2021-03-22 13:35:24 +0200

    task 7

--- React-test.js
+++ React-test.js
@@ -108,7 +108,44 @@ class ShoppingCart extends React.Component {
   }
 };
 
-//
+//Access Props Using this.props
+    
+class ReturnTempPassword extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+        <div>
+            { /* Change code below this line */ }
+            <p>Your temporary password is: 
+              <strong>
+                {this.props.tempPassword}
+              </strong>
+            </p>
+            { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+        <div>
+          <h2>Reset Password</h2>
+          <h3>We've generated a new temporary password for you.</h3>
+          <h3>Please reset this password from your account settings ASAP.</h3>
+          { /* Change code below this line */ }
+           <ReturnTempPassword tempPassword = "8characterslong"/>
+          { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
     
     
     
