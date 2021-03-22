f2eb72fbe27bfc5b6f98ee0c2049a05810bd2565
advya <50411324+Advya@users.noreply.github.com>
2021-03-22 19:10:31 +0200

    task 21

--- React-test.js
+++ React-test.js
@@ -567,6 +567,48 @@ class MyComponent extends React.Component {
   }
 }
 
-//Use the Lifecycle Method componentDidMount=======================================================================================
+//Optimize Re-Renders with shouldComponentUpdate=======================================================================================
 
+class OnlyEvens extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Should I update?');
+    // Change code below this line
+    if( nextProps.value % 2 == 0) {
+      return true;
+    }else {return false;}
+    // Change code above this line
+  }
+  componentDidUpdate() {
+    console.log('Component re-rendered.');
+  }
+  render() {
+    return <h1>{this.props.value}</h1>;
+  }
+}
+
+class Controller extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+    this.addValue = this.addValue.bind(this);
+  }
+  addValue() {
+    this.setState(state => ({
+      value: state.value + 1
+    }));
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.addValue}>Add</button>
+        <OnlyEvens value={this.state.value} />
+      </div>
+    );
+  }
+}
 
